42558f804ab691f5fcc5c88c5d6fc41a9d33ff8a
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-11-09 00:04:48 +0100

    Implement placeholder methods for ImageServiceServer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"strconv"
 
 	"google.golang.org/grpc"
 
@@ -23,19 +24,55 @@ type RuntimeServiceServer struct {
 // Version serves `crictl version`
 func (r *RuntimeServiceServer) Version(ctx context.Context, req *cri.VersionRequest) (*cri.VersionResponse, error) {
 	log.Printf("Received request on Version with input %s", req.GetVersion())
-	resp := cri.VersionResponse{
+	return &cri.VersionResponse{
 		Version:           req.GetVersion(),
 		RuntimeName:       "go-dumbcri",
 		RuntimeVersion:    "v0.0.0-alpha.1",
 		RuntimeApiVersion: "v0.0.0-alpha.1",
-	}
-	return &resp, nil
+	}, nil
 }
 
 type ImageServiceServer struct {
-	// Needed to compile even if some methods are not implemented here
-	// See https://stackoverflow.com/questions/40823315/x-does-not-implement-y-method-has-a-pointer-receiver
-	cri.UnimplementedImageServiceServer
+	Images []*cri.Image
+}
+
+// ListImages serves `crictl images`
+func (i *ImageServiceServer) ListImages(ctx context.Context, req *cri.ListImagesRequest) (*cri.ListImagesResponse, error) {
+	log.Printf("Received request on ListImages with Filter=%s", req.GetFilter())
+	return &cri.ListImagesResponse{
+		Images: i.Images,
+	}, nil
+}
+
+// ImageStatus serves `crictl inspecti`
+func (i *ImageServiceServer) ImageStatus(ctx context.Context, req *cri.ImageStatusRequest) (*cri.ImageStatusResponse, error) {
+	log.Printf("Received request on ImageStatus with Image=%s Verbose=%s", req.GetImage(), strconv.FormatBool(req.GetVerbose()))
+	return &cri.ImageStatusResponse{
+		Image: &cri.Image{},
+		Info: map[string]string{
+			"key": "value",
+		},
+	}, nil
+}
+
+// PullImage serves `crictl pull`
+func (i *ImageServiceServer) PullImage(ctx context.Context, req *cri.PullImageRequest) (*cri.PullImageResponse, error) {
+	log.Printf("Received request on PullImage with Image=%s Auth=%s SandboxConfig=%s", req.GetImage(), req.GetAuth(), req.GetSandboxConfig())
+	return &cri.PullImageResponse{}, nil
+}
+
+// RemoveImage serves `crictl rmi`
+func (i *ImageServiceServer) RemoveImage(ctx context.Context, req *cri.RemoveImageRequest) (*cri.RemoveImageResponse, error) {
+	log.Printf("Received request on RemoveImage with Image=%s", req.GetImage())
+	return &cri.RemoveImageResponse{}, nil
+}
+
+// ImageFsInto serves `crictl imagefsinfo`
+func (i *ImageServiceServer) ImageFsInfo(ctx context.Context, req *cri.ImageFsInfoRequest) (*cri.ImageFsInfoResponse, error) {
+	log.Printf("Received request on ImageFsInfo")
+	return &cri.ImageFsInfoResponse{
+		ImageFilesystems: []*cri.FilesystemUsage{},
+	}, nil
 }
 
 func main() {
