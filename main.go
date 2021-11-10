f2f11bb0ed22d2a052b26b79ada0e8e484f3c73e
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-11-11 00:21:19 +0100

    Unlink socket before binding it

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"strconv"
+	"syscall"
 
 	"google.golang.org/grpc"
 
@@ -12,6 +13,10 @@ import (
 	// See https://github.com/kubernetes-sigs/cri-tools/blob/36e98a6/cmd/crictl/version.go#L30
 	// See https://github.com/kubernetes-sigs/cri-tools/pull/712
 	cri "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+)
+
+const (
+	socketaddr = "/tmp/go-dumbcri.sock"
 )
 
 // https://github.com/kubernetes/kubernetes/blob/41ab2cc/staging/src/k8s.io/cri-api/pkg/apis/runtime/v1/api.proto
@@ -81,10 +86,16 @@ func main() {
 	cri.RegisterRuntimeServiceServer(server, &RuntimeServiceServer{})
 	cri.RegisterImageServiceServer(server, &ImageServiceServer{})
 
+	// We might want to acquire a lock instead of unlinking
+	// See https://gavv.github.io/articles/unix-socket-reuse/
+	err := syscall.Unlink(socketaddr)
+	if err != nil {
+		log.Printf("Failed to unlink: %v", err)
+	}
 	// See https://stackoverflow.com/questions/2886719/unix-sockets-in-go
-	lis, err := net.Listen("unix", "/tmp/go-dumbcri.sock")
+	lis, err := net.Listen("unix", socketaddr)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
-	server.Serve(lis)
+	log.Print(server.Serve(lis))
 }
